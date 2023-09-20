ead59d16995eb2f4c98af13120ff227e4404c15b
ivanjorge <ivandcarvalho29@gmail.com>
2023-09-20 21:46:11 +0100

    Trying to make each smallBoard check the functions

--- js/Ultimate_TicTacToe.js
+++ js/Ultimate_TicTacToe.js
@@ -1,124 +1,176 @@
-  //!Problems to solve
-  //!declare winner
-  //!solve clicks to every cell
-  //!make jump from one small board to another according to chosen cell index
- 
+//! CURRENT BLOCKERS
+//? -BOARD DOESN'T LOCK TO THE SMALLBOARD W/ VALUE OF THE CELL CLICKED
+//? -IT DOENS'T ACTUALIZE THE CURRENTSMALLBOARD BEING PLAYED
+//? -THE initGAME FUNCTION DOESN'T RESET THE BOARD COMPLETELY
+
 /*----- constants -----*/
 const cells = document.querySelectorAll('.cells');
-const smallBoards = document.querySelectorAll('.small-board');
+const smallBoards = document.querySelectorAll('.boards');
 const gameStatus = document.querySelector('.status');
-const symbols = { '0': null, 'X': 'X', 'O': 'O',};
 const rules = document.querySelectorAll('#rules p, #rules li');
 const playAgain = document.querySelector('.reset');
+const winCombos = [];
 
 /*----- state variables -----*/
 let currentPlayer = 'X';
-let boardState = [    
-    ['', '', '', '', '', '', '', '', ''], // Cell 0 to 8
-    ['', '', '', '', '', '', '', '', ''], // Cell 9 to 17
-    ['', '', '', '', '', '', '', '', ''], // Cell 18 to 26
-    ['', '', '', '', '', '', '', '', ''], // Cell 27 to 35
-    ['', '', '', '', '', '', '', '', ''], // Cell 36 to 44
-    ['', '', '', '', '', '', '', '', ''], // Cell 45 to 53
-    ['', '', '', '', '', '', '', '', ''], // Cell 54 to 62
-    ['', '', '', '', '', '', '', '', ''], // Cell 63 to 71
-    ['', '', '', '', '', '', '', '', ''], // Cell 72 to 80
-];
-let smallBoardState = ['', '', '', '', '', '', '', '', ''];
-let activeSmallBoard = null;
+let smallBoardState = Array(81).fill('');
+let boardState = Array(9).fill('') ;
+let currentSmallBoard = null;
 
+// calling functions
+initGame();
 
-/*----- cached elements  -----*/
-
-
-
-/*----- event listeners -----*/
-// To make the Game Rules btn hide and display the rules text
-document.getElementById('toggle-rules').addEventListener('click', function() {
-    rules.forEach(rule => {
-        rule.classList.toggle('hidden');
-    });
-});
-
-//To reset game
-playAgain.addEventListener('click', resetGame);
-
-
-// To check for a win
-function checkWin(player, board) {
-    const winCombos = [
-        [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
-        [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
-        [0, 4, 8], [2, 4, 6]             // Diagonals
-    ];
-
-    // Convert winCombos to use data-value attributes instead of indexes
-    const winCombosWithValues = winCombos.map(combo => combo.map(index => cells[index].getAttribute('data-value')));
-
-    // Use the 'some method to iterate over every winning combo
-    // If it finds a combination containing the same player's symbols, it returns true (win)
-    return winCombosWithValues.some(combo => combo.every(value => board[value] === player));
-}
-
-
-// To handle player movement when a cell is clicked
-cells.forEach((cell, index) => {
-    cell.addEventListener('click', () => {
-        console.log('cell clicked', index);
-        console.log('current player', currentPlayer);
-        console.log('cell content before', cell.textContent);
-        if (activeSmallBoard === null || activeSmallBoard === index) {
-            if (boardState[index] === '' && !isGameOver()) {
-                cell.textContent = currentPlayer;
-                console.log('cell content after', cell.textContent);
-                boardState[index] = currentPlayer;
-                smallBoardState[index] = currentPlayer;
-                
-                currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-                
-                if (checkWin(currentPlayer, smallBoardState)) {
-                    gameStatus.textContent = `${currentPlayer} wins the small board!`;
-                    smallBoards[index].textContent = `${currentPlayer}`
-                } else if (isSmallBoardFull(smallBoardState)) {
-                    gameStatus.textContent = "It's a draw!";
-                    smallBoards[index].textContent = null;
-                } else {
-                    gameStatus.textContent = `${currentPlayer}'s Turn`;
-                    //activeSmallBoard = index; // Update the active small board to the current cell.
-                }
-
-            }
-        }
-    });
-});
 
 /*----- functions -----*/
-
-
-// To check if small board is full
-function isSmallBoardFull(board) {
-    // Again use the 'every method to check every cell in the board
-    //if the cells are filled, it returns true, if not, returns false
-    return board.every(cell => cell !== '');
+function addEventListeners() {
+  // to toggle rules
+  document.getElementById('toggle-rules').addEventListener('click', toggleRules);
+  // To reset game
+  playAgain.addEventListener('click', initGame);
 }
 
-// To check if the game is over
-function isGameOver() {
-    // Call isSmallBoardFull to check if all small boards are full
-    // Call checkWin (player, smallBoardState) to check if X or O has won the small board
-    // If any of these conditions are met, it returns true, if not, false
-    return isSmallBoardFull(boardState) || checkWin('X', smallBoardState) || checkWin('O', smallBoardState);
+function toggleRules() {
+  rules.forEach(rule => {
+    rule.classList.toggle('hidden');
+  });
 }
 
-// To reset game
-function resetGame() {
-    currentPlayer = 'X';
-    boardState = ['', '', '', '', '', '', '', '', ''];
-    smallBoardState = ['', '', '', '', '', '', '', '', ''];
-    activeSmallBoard = null;
-    gameStatus.textContent = `${currentPlayer}'s Turn`;
+// To initialize / reset game
+function initGame() {
+  currentPlayer = 'X';
+  smallBoardState.fill('');
+  boardState.fill('');
+  currentSmallBoard = null;
+  clearBoard(cells);
+  gameStatus.innerHTML = `${currentPlayer}'s Turn`;
+  addEventListeners();
+}
 
-    cells.forEach(cell => {
-        cell.textContent = '';
-    });
+function clearBoard(boardElements) {
+  boardElements.forEach(board => {
+    board.textContent = '';
+  });
+}
+//<<<<<<<<<<<<<<<<<<<< CHECK CELL CLICKS >>>>>>>>>>>>>>>>>>>>
+
+// Add this function to your code to handle cell clicks
+function cellClickHandler(event) {
+    const cell = event.target;
+    console.log(`1st click by ${currentPlayer}`);
+    // Check if the cell is in the allowed small board and is empty
+    const cellIndex = Array.from(cells).indexOf(cell);
+    if (cellIndex === -1 || smallBoardState[cellIndex] !== '' || currentSmallBoard !== null) {
+      return; // Cell is not allowed to be clicked
+    }
+    // Update the cell's text content with the current player's symbol ('X' or 'O')
+    cell.textContent = currentPlayer;
+    
+
+    
+    // Update the small board and overall board state
+    gameStatus.innerHTML = `${currentPlayer}'s Turn`;
+    smallBoardState[cellIndex] = currentPlayer;
+    boardState[cellIndex + (currentSmallBoard * 9)] = currentPlayer;
+  
+    
+    console.log(`2nd click by ${currentPlayer}, cell: ${cellIndex}, board ${currentSmallBoard}`);
+    
+    if (checkSmallBoardWin(currentPlayer, currentSmallBoard)) {
+        markSmallBoard(currentPlayer, currentSmallBoard);
+    }
+    
+    if (checkSmallBoardDraw(currentPlayer, currentSmallBoard)) {
+        gameStatus.innerHTML = `board ${currentSmallBoard} It\'s a Draw`;
+        
+    }
+    
+    // Toggle the current player
+    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+
+    // Remove the click event listener from the clicked cell
+    cell.removeEventListener('click', cellClickHandler);
+  }
+  
+  // Add a click event listener to each cell
+  cells.forEach(cell => {
+    cell.addEventListener('click', cellClickHandler);
+  });
+  
+  //<<<<<<<<<< SMALL BOARD WIN >>>>>>>>>>
+   function checkSmallBoardWin(player, smallBoardIndex) {
+    for (let i = 0; i < winCombos.length; i++) {
+      const [a, b, c] = winCombos[i];
+      if (
+        smallBoardState[smallBoardIndex][a] === player &&
+        smallBoardState[smallBoardIndex][b] === player &&
+        smallBoardState[smallBoardIndex][c] === player
+      ) {
+        
+        return true;
+      }
+    }
+    return false;
+  }
+  //<<<<<<<<<< SMALL BOARD DRAW >>>>>>>>>>
+  function checkSmallBoardDraw(smallBoardIndex) {
+    if (symbols.some(symbol => symbol === '')) {
+      return false;
+    }
+   
+    if (checkSmallBoardWin('X', smallBoardIndex) || checkSmallBoardWin('O', smallBoardIndex)) {
+      return false;
+    }
+    return true;
+  }
+   //<<<<<<<<<< MARK SMALL BOARD >>>>>>>>>>
+   function markSmallBoard(player, smallBoardIndex) {
+    const cellsInSmallBoard = cells;
+  
+    for (let i = smallBoardIndex * 9; i < (smallBoardIndex + 1) * 9; i++) {
+        cellsInSmallBoard[i].textContent = player;
+    }
+  }
+  
+//<<<<<<<<<< CHECK GAME WIN >>>>>>>>>>
+function checkGameWin(player) {
+    for (let i = 0; i < winCombos.length; i++) {
+      const [a, b, c] = winCombos[i];
+      if (
+        boardState[a] === player &&
+        boardState[b] === player &&
+        boardState[c] === player
+      ) {
+        // Handle game win here
+        return true;
+      }
+    }
+    return false;
+  }
+  
+
+
+  //<<<<<<<<<< WINNING COMBOS >>>>>>>>>>
+
+  // Rows
+for (let i = 0; i < 9; i++) {
+  for (let j = 0; j < 9; j++) {
+    winCombos.push([i * 9 + j, i * 9 + j + 1, i * 9 + j + 2]);
+  }
+}
+
+// Columns
+for (let i = 0; i < 9; i++) {
+  for (let j = 0; j < 9; j++) {
+    winCombos.push([i + j * 9, i + (j + 1) * 9, i + (j + 2) * 9]);
+  }
+}
+
+// Diagonals (top-left to bottom-right)
+for (let i = 0; i < 9; i++) {
+  winCombos.push([i * 9 + i, (i + 1) * 9 + i + 1, (i + 2) * 9 + i + 2]);
+}
+
+// Diagonals (top-right to bottom-left)
+for (let i = 0; i < 9; i++) {
+  winCombos.push([i * 9 + (8 - i), (i + 1) * 9 + (7 - i), (i + 2) * 9 + (6 - i)]);
 }
